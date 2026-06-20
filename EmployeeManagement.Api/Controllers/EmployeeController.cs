c9f2f53138d433077b66f238d13e9c765372b170
raisahmad6 <raisahmad1982@gmail.com>
2026-06-21 00:55:17 +0530

    First change

---
+++ EmployeeManagement.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Api.Controllers
+{
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new[]
+        {
+            new { Id = 1, Name = "John" },
+            new { Id = 2, Name = "David" }
+        });
+    }
+
+}
+}
